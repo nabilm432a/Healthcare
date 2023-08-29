c60271ac9fd518b2e7fc1dd1232be870a5335f15
Phantom <141241567+nabilm432a@users.noreply.github.com>
2023-08-29 22:00:33 +0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,3 @@
-function updateGreeting() {
-    var now = new Date();
-    var hour = now.getHours();
-
-    var greeting = "";
-    if (hour >= 0 && hour < 12) {
-      greeting = "Good morning!";
-    } else if (hour >= 12 && hour < 18) {
-      greeting = "Good afternoon!";
-    } else {
-      greeting = "Good evening!";
-    }
-
-    document.getElementById("greeting").textContent = greeting;
-  }
-
 function validatetestform() {
     var testDropdown = document.getElementById("test");
     var selectedTest = testDropdown.options[testDropdown.selectedIndex].text;
